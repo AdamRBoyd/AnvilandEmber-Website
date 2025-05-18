bcf40b0139a273173a45a27d159616d67a2aa099
Adam Boyd <aboyd08@gmail.com>
2025-05-18 13:43:51 -0700

    Updating information AboutMe

--- src/components/organisms/AboutMe/index.jsx
+++ src/components/organisms/AboutMe/index.jsx
@@ -80,55 +80,44 @@ const AboutMe = () => {
       <ProfileImage src='/images/ProfilePic.png' />
       <RightSideTextBlock>
         <RightParagraph>
-          Welcome to my portfolio website! I am a jewelry designer and
-          silversmith with a passion for creating unique and meaningful pieces
-          of jewelry.
+          Welcome! I am a jewelry designer and silversmith with a passion for creating unique and meaningful pieces of jewelry.
         </RightParagraph>
         <RightParagraph>
           I am the founder of{' '}
-          <Link href={'https://AdamBoydDesigns.etsy.com'} target='_blank'>
-            {' AdamBoydDesigns on Etsy'}
+          <Link href={'https://AnvilandEmberMetalworks.etsy.com'} target='_blank'>
+            {' Anvil and Ember on Etsy'}
           </Link>
-          , where I sell my handcrafted silver and copper jewelry. I enjoy using
-          my creativity and attention to detail to design and create beautiful
-          and functional pieces of jewelry. From simple and elegant designs to
-          more intricate and personalized pieces, I strive to create something
-          that speaks to each individual wearer.
+          , where I sell my handcrafted silver and copper jewelry. I enjoy using my creativity and attention to detail to design and create beautiful and functional pieces of jewelry. From simple and elegant designs to
+          more intricate and personalized pieces, I strive to create something that speaks to each individual wearer.
         </RightParagraph>
         <RightParagraph>
-          In addition to my work in jewelry design, I also have a background in
-          computers, fabrication, and graphic design. I recently graduated from
-          California State University, East Bay with a Bachelor of Science in
-          Computer Science and have experience in developing websites using
-          HTML, CSS, JavaScript, React and Nodejs. Additionally I have
-          experience with several programming languages including C++, Java,
-          Python, and more.
+          In addition to my work in jewelry design, I also have a background in computers, fabrication, and graphic design. I recently graduated from
+          California State University, East Bay with a Bachelor of Science in Computer Science and have experience in developing websites using
+          HTML, CSS, JavaScript, React and Nodejs. Additionally I have experience with several programming languages including C++, Java, Python, and more.
         </RightParagraph>
         <RightParagraph>
-          In the "Code" section of my website, you will find a link to my
+          In the 
+          <Link to='/code'>{' Code '}</Link>
+          section of my website, you will find a link to my
           <Link
-            href={'https://github.com/phoenix239/adamboyddesigns'}
+            href={'https://github.com/AdamRBoyd'}
             target='_blank'
           >
             {' GitHub '}
           </Link>
-          profile, as well as many mini projects that I have developed using
-          various programming languages and tools. I enjoy exploring new
-          technologies and creating applications that are both functional and
-          visually appealing.
+          profile, as well as many mini projects that I have developed using various programming languages and tools. I enjoy exploring new
+          technologies and creating applications that are both functional and visually appealing.
         </RightParagraph>
         <RightParagraph>
-          Whether I am creating jewelry or programming, I bring the same level
-          of passion and attention to detail to everything I do. I am always
-          looking for new opportunities to grow and learn in both fields, and I
-          am excited to share my work with others. Thank you for visiting my
+          Whether I am creating jewelry or programming, I bring the same level of passion and attention to detail to everything I do. I am always
+          looking for new opportunities to grow and learn in both fields, and I am excited to share my work with others. Thank you for visiting my
           portfolio website, and please feel free to
           <Link to='/contact'>{' contact me'}</Link> if you have any questions
           or if you are interested in working together.
         </RightParagraph>
         <Spacer padding={2} />
         <InstagramLink
-          href='https://www.instagram.com/adamboyddesigns/'
+          href='https://www.instagram.com/AnvilandEmberMetalworks/'
           target='_blank'
         >
           <InstagramBlock>
